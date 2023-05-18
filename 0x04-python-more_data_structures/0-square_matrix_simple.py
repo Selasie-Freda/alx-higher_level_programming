b07e7d4154e3ab8e4610d41a16de8a77755e8d34
Selasie-Freda <siesela88@gmail.com>
2023-05-17 17:19:34 -0700

    square mat

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,20 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared = []
-    if matrix is None:
-        return None
-    if not matrix:
-        return None
-    for lists in matrix:
-        for num in lists:
-            num **= 2
-            squared.append(num)
-
-    r_1 = squared[:3]
-    r_2 = squared[3:6]
-    r_3 = squared[-3:]
-    column = []
-    column.append(r_1)
-    column.append(r_2)
-    column.append(r_3)
-    return column
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
