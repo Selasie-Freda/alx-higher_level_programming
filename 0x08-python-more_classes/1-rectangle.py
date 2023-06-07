5c1d70331604d6ec621736117bb7cac80bd36890
Selasie-Freda <siesela88@gmail.com>
2023-06-07 03:14:50 -0700

    correction

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,34 +1,47 @@
 #!/usr/bin/python3
-"""class Rectangle that defines a rectangle"""
+"""This module creates a rectangle"""
 
 
 class Rectangle:
-    """defines a rectangle"""
-
+    """This is the class Rectangle. This time it's necessary to define some
+    attributes"""
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
 
-        @width.setter
-        def width(self, value):
-            if isinstance(value, int) is False:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
             self.__width = value
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def height(self):
+        return(self.__height)
 
-        @height.setter
-        def height(self, value):
-            if isinstance(value, int) is False:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
             self.__height = value
-
-        @property
-        def height(self):
-            return self.__height
