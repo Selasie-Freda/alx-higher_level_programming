a50cb00e734505c65d030c16b201141f53ce18c4
Selasie-Freda <siesela88@gmail.com>
2023-06-08 02:20:17 -0700

    max int

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python3
-"""Module to find the max integer in a list
+#!/usr/bin/bash/python3
+"""Unittest for max_integer([...])
 """
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxInteger (unittest.TestCase):
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer, (45, 54, 728), 728)
+        self.assertEqual(max_integer,(), None)
+        self.assertEqual(max_integer,("perro" ))
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+if __name__ == '__main__':
+    unittest.main()
